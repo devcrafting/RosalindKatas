bd062153e15ececdc16cf707cb244f3ee8f1f7e7
Cl�ment Bouillier <clement.bouillier@devcrafting.com>
2015-06-13 09:49:53 +0200

    Init iteration1

---
+++ CodeRetreatJune2015/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    let fileContent = File.ReadAllText(argv.[0])
+    File.WriteAllText(argv.[0] + "_out", fileContent)
+    0 // return an integer exit code
