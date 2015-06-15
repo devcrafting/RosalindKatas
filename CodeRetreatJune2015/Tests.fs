29c78382244fe815eeea2b07e034b5f293201914
Clément Bouillier <clement.bouillier@devcrafting.com>
2015-06-15 21:36:40 +0200

    Test for iteration 2 : reverse, and filter still working

--- CodeRetreatJune2015/Tests.fs
+++ CodeRetreatJune2015/Tests.fs
@@ -9,3 +9,15 @@ let ``Given a sequence with unknown nucleotides, when filter, then remove unknow
     let sequence = "ATUGACRE".ToCharArray() |> Array.toList
     let filtered = filterSequence nothing List.Empty sequence ' ' 0
     filtered |> System.String.Concat |> should equal "ATGAC"
+
+[<Fact>]
+let ``Given a sequence, when reverse, then return reverse sequence`` () =
+    let sequence = "ATGAC".ToCharArray() |> Array.toList
+    let filtered = filterSequence reverse List.Empty sequence ' ' 0
+    filtered |> System.String.Concat |> should equal "CAGTA"
+    
+[<Fact>]
+let ``Given a sequence with unknown nucleotides, when reverse, then return reverse sequence without unknown nucleotides`` () =
+    let sequence = "ATUGACRE".ToCharArray() |> Array.toList
+    let filtered = filterSequence reverse List.Empty sequence ' ' 0
+    filtered |> System.String.Concat |> should equal "CAGTA"
