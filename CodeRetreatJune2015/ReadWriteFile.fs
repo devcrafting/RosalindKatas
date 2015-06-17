1cbb296e7183666582aae20e402634efeb52be5d
Clément Bouillier <clement.bouillier@devcrafting.com>
2015-06-17 07:41:14 +0200

    Extract a readInput function to extract InputOutput type from string array (read from file)

---
+++ CodeRetreatJune2015/ReadWriteFile.fs
@@ -0,0 +1,13 @@
+﻿module ReadWrite
+
+open NDA
+
+let readInput (fileContent: string array) =
+        let input =
+            if fileContent.Length > 1 then (fileContent.[0], fileContent.[1]) else ("nothing", fileContent.[0])
+        let instruction, input = input
+    
+        match instruction with
+            | "reverse" -> Reverse { Input = input; Output = List.Empty }
+            | "count GA" -> Count { Input = input; Pattern = ['G'; 'A']; Output = (Seq.empty, 0) }
+            | _ -> Nothing { Input = input; Output = List.Empty }
---
+++ CodeRetreatJune2015/ReadWriteTests.fs
@@ -0,0 +1,35 @@
+﻿module ReadWriteTests
+
+open FsUnit
+open Xunit
+open ReadWrite
+open NDA
+
+[<Fact>]
+let ``Given input without instruction, when read input, then return Nothing InputOutput`` () =
+    let input = [|"ACGTC"|]
+    let inputOutput = readInput input
+    match inputOutput with
+    | Nothing inputOutput -> 
+        inputOutput.Input |> should equal input.[0]
+        inputOutput.Output |> Seq.length |> should equal 0
+        
+[<Fact>]
+let ``Given input with Reverse instruction, when read input, then return Reverse InputOutput`` () =
+    let input = [|"reverse";"ACGTC"|]
+    let inputOutput = readInput input
+    match inputOutput with
+    | Reverse inputOutput -> 
+        inputOutput.Input |> should equal input.[1]
+        inputOutput.Output |> Seq.length |> should equal 0
+        
+[<Fact>]
+let ``Given input with Count instruction with pattern GA, when read input, then return Count InputOutput`` () =
+    let input = [|"count GA";"ACGTC"|]
+    let inputOutput = readInput input
+    match inputOutput with
+    | Count inputOutput -> 
+        inputOutput.Input |> should equal input.[1]
+        inputOutput.Pattern |> should equal ['G';'A']
+        fst inputOutput.Output |> Seq.length |> should equal 0
+        snd inputOutput.Output |> should equal 0
